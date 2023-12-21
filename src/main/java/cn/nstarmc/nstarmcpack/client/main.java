5d1da05175a70c78be3264801851cffdb615e944
luluxiaoyu <ljy.xiaoyu@qq.com>
2023-12-21 21:46:11 +0800

    fix

--- src/main/java/cn/nstarmc/nstarmcpack/client/main.java
+++ src/main/java/cn/nstarmc/nstarmcpack/client/main.java
@@ -1,2 +1,18 @@
-package cn.nstarmc.nstarmcpack.client;public class main {
+package cn.nstarmc.nstarmcpack.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+public class main implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.START_CLIENT_TICK.register(client -> {
+            long window = MinecraftClient.getInstance().getWindow().getHandle();
+            GLFW.glfwSetWindowTitle(window, "NSTARMC ModPack - by luluxiaoyu");
+        });
+    }
 }
--- src/main/java/cn/nstarmc/nstarmcpack/main.java
+++ src/main/java/cn/nstarmc/nstarmcpack/main.java
@@ -1,2 +1,14 @@
-package cn.nstarmc.nstarmcpack;public class main {
+package cn.nstarmc.nstarmcpack;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.text.Text;
+
+public class main implements ModInitializer {
+    public void onInitialize() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            handler.player.sendMessage(Text.of("欢迎使用NSTARMC系列基础模组整合包！by luluxiaoyu"), false);
+            handler.player.sendMessage(Text.of("QQ群：796440132"), false);
+        });
+    }
 }
